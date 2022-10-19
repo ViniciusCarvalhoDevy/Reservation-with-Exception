e5e767a2f42f19f3ecae5754bfbfb25c02f09449
Vinicius Carvalho <114104425+ViniciusCarvalhoDevy@users.noreply.github.com>
2022-10-19 15:28:19 -0300

    Create ClientUser.java

---
+++ Class/ClientUser.java
@@ -0,0 +1,65 @@
+public class ClientUser {
+    private  int diaEntrada ;
+    private int mesEntrada;
+    private int anoEntrada;
+    private  int diaSaida;
+    private int mesSaida;
+    private int anoSaida;
+    private int numeroQ;
+    private int cod;
+
+public int getCod() {
+        return cod;
+    }
+
+
+
+public ClientUser(int cod){
+    this.cod = cod;
+}
+
+
+
+public int getDiaEntrada() {
+    return diaEntrada;
+}
+public void setDiaEntrada(int diaEntrada) {
+    this.diaEntrada = diaEntrada;
+}
+public int getMesEntrada() {
+    return mesEntrada;
+}
+public void setMesEntrada(int mesEntrada) {
+    this.mesEntrada = mesEntrada;
+}
+public int getAnoEntrada() {
+    return anoEntrada;
+}
+public void setAnoEntrada(int anoEntrada) {
+    this.anoEntrada = anoEntrada;
+}
+public int getDiaSaida() {
+    return diaSaida;
+}
+public void setDiaSaida(int diaSaida) {
+    this.diaSaida = diaSaida;
+}
+public int getMesSaida() {
+    return mesSaida;
+}
+public void setMesSaida(int mesSaida) {
+    this.mesSaida = mesSaida;
+}
+public int getAnoSaida() {
+    return anoSaida;
+}
+public void setAnoSaida(int anoSaida) {
+    this.anoSaida = anoSaida;
+}
+public int getNumeroQ() {
+    return numeroQ;
+}
+public void setNumeroQ(int numeroQ) {
+    this.numeroQ = numeroQ;
+}
+    }
